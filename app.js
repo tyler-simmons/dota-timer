3ada7ee241f350d517cb1ca77b9e478c1efe6e1e
Tyler Simmons <tylersimmons@Tylers-MBP.fios-router.home>
2019-02-10 02:53:48 -0500

    Server running with landing page

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 var express = require("express"),
-	app = express();
+		app = express();
 
+	
+	app.use(express.static(__dirname + "/public"));
+	app.set('view-engine', 'ejs');
 
-	app.get("/", function(req,res) {
-		res.send("Welcome - root route works");
+	app.get("/", function(req, res){
+		res.render("landing.ejs");
 	});
 
-	app.listen(process.env.PORT, process.env.IP, function(){
-		console.log("Server started");
-	});
+	app.listen(3000, function(){
+		console.log("Server started successfully");
+	});	
