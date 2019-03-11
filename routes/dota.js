bd6f173c409a619b5e17a8d4edfffb782342fadb
Tyler Simmons <tylersimmons@Tylers-MBP.fios-router.home>
2019-03-11 16:08:27 -0400

    test

--- routes/dota.js
+++ routes/dota.js
@@ -4,9 +4,7 @@ var cors = require('cors');
 var router = express.Router();
 
 router.get('/', cors(), function(req, res){
-    res.sendFile('timer.JSON', {
-        root: '/data'
-    });
+    res.sendFile('timer.JSON');
 });
 
 router.get('/test', function(req, res){
